3651e24859f8f952a1bd409472c5a7c347e305ca
aakash <aakashveerappan@gmail.com>
2021-01-23 16:07:39 +0530

    changed text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React JS</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js learn react</code> and save to reload.
